ab11eb06102417bc2e052fb576074703da1a70f0
khoiriahazizahh <khoiriahazizah@student.ub.ac.id>
2025-05-31 21:41:02 +0700

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,13 +1,21 @@
-package uap.uap.bases;
+package uap.uap.bases; 
 
 public abstract class Shape {
+    // Atribut privat untuk menyimpan nama bentuk
     private String name;
 
+    // Method setter untuk mengatur nama bentuk
     public void setName(String name) {
         this.name = name;
     }
 
+    // Method abstrak untuk menghitung volume
     public abstract double getVolume();
+
+    // Method abstrak untuk menghitung luas permukaan
     public abstract double getSurfaceArea();
+
+    // Method abstrak untuk mencetak informasi bentuk
     public abstract void printInfo();
 }
+
